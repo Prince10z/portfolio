5ba171a339b8cb5c19577b3aca453283895d9f4d
Piyush <Piyush22122@gmail.com>
2024-06-08 15:08:51 +0530

    changed url path

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const sendingUrl = "https://backendportfolio-1dz3.onrender.com/resume/send";
 const headerbackground = document.getElementById("fixedheader")
 const arrows = document.getElementsByClassName("arrow");
 window.addEventListener('scroll', function () {
@@ -46,6 +47,7 @@ const subBut = document.getElementById("submitForm");
 subBut.addEventListener("click", (e) => {
     e.preventDefault();
     // submitForm();
+    submitForms();
 });
 subBut.addEventListener("mouseup", () => {
     subBut.style.boxShadow = "1px 1px 9px rgba(136, 111, 165, 0.3)";
@@ -55,47 +57,94 @@ subBut.addEventListener("mousedown", () => {
     subBut.style.boxShadow = "1px 1px 9px rgba(136, 111, 165, 0.9)";
 });
 
+function ValidateEmail(input) {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (input.match(validRegex)) {
 
 
-function submitForm() {
-    console.log("testinfasdf");
-    // Retrieve form data
-    const fullname = document.getElementById('fullname').value;
-    const email = document.getElementById('email').value;
-    const msg = document.getElementById('msg').value;
+        document.getElementById("email").focus();
+
+        return true;
+
+    } else {
+
+
+        document.getElementById("email").focus();
+
+        return false;
+
+    }
+
+}
+
+function submitForms() {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-    // Construct the form data
-    const formData = new FormData();
-    const urlencoded = new URLSearchParams();
-    urlencoded.append("fullname", fullname);
-    urlencoded.append("email", email);
-    urlencoded.append("msg", msg);
-
-    // Make a POST request
-    fetch('http://192.168.29.219:3000/resume/send', {
-        method: 'POST',
-        headers: myHeaders,
-        body: urlencoded
-    })
-        .then(response => {
-            console.log(response);
-            if (!response.ok) {
-                alert("An error occurred. Please try again later.");
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            // Handle response
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            // Handle error
+    const namevalue = document.getElementById("fullname").value;
+    const emailvalue = document.getElementById("email").value;
+    const msgvalue = document.getElementById("msg").value;
+    if (namevalue.trim() == "" || emailvalue.trim() == "" || msgvalue.trim() == "") {
+        showAlert("First fill the required info");
+        console.log(namevalue.trim() == "" || emailvalue.trim() == "" || msgvalue.trim());
+        if (namevalue.trim() == "") {
+            document.getElementById("fullname").focus();
+        }
+        if (emailvalue.trim() == "") {
+            document.getElementById("email").focus();
+        }
+        if (msgvalue.trim() == "") {
+            document.getElementById("msg").focus();
+        }
+    } else {
+        console.log("test2");
+        if (!ValidateEmail(emailvalue)) {
+            showAlert("Invalid email address.");
+            document.getElementById("email").focus();
+            return; // Exit function if email is invalid
+        }
+        const bodyval = new URLSearchParams({
+            fullname: namevalue,
+            email: emailvalue,
+            msg: msgvalue
         });
+
+        const reqOpt = {
+            method: "POST",
+            headers: myHeaders,
+            body: bodyval.toString(),
+        };
+
+        fetch(sendingUrl, reqOpt)
+            .then((response) => {
+                if (!response.ok) {
+                    if (response.status == 500) {
+                        showAlert("Please check your internet connection");
+                        throw new Error("Please check your internet connection");
+                    } else if (response.status == 400) {
+                        showAlert("First fill the required info");
+                        throw new Error("First fill the required info");
+                    } else {
+                        throw new Error("An unexpected error occurred");
+                    }
+                }
+                return response.json();
+            })
+            .then((response) => {
+                showAlert("Successfully send");
+                console.log(response.status); // Assuming response contains a status property
+            })
+            .catch((e) => {
+                showAlert(`Please try again`);
+                console.error("Error in sending message:", e.message);
+            });
+
+
+    }
+
 }
+
 
 //Alert..........
 const overlay = document.getElementById("overlay");
